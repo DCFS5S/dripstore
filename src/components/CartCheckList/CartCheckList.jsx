c16daffe5e62e664449eda7c43afa74acd59ccf9
Flávia Reis <flaviarster@gmail.com>
2023-11-16 20:14:14 -0300

    Reorganizei o componente e o estilo

--- src/components/CartCheckList/CartCheckList.jsx
+++ src/components/CartCheckList/CartCheckList.jsx
@@ -1,29 +1,40 @@
 import './CartCheckList.css'
 
-export function CartCheckList() {
+export function CartCheckList(props) {
     return (
-        <div className='container'>
-            <div className='menu'>
-                <ul>
-                    <li>MEU CARRINHO</li>
-                    <li>QUANTIDADE</li>
-                    <li>UNITÁRIO</li>
-                    <li>TOTAL</li>
-                </ul>
-            </div>
-            <div className='produto'>
-                <div className='imagem'>
-                    <img src="" alt="" />
-                </div>
-                <div className='conteudo'>
-                    <div className='descricao'>
-                        <h4>Tênis Nike Revolution 6 Next Nature Masculino</h4>
-                        <p>Cor: Vermelho / Branco</p>
-                        <p>Tamanho: 42</p>
-                    </div>
-                </div>
-            </div>
+        <div>
+            <table>
+                <thead>
+                    <th>MEU CARRINHO</th>
+                    <th></th>
+                    <th>QUANTIDADE</th>
+                    <th>UNITÁRIO</th>
+                    <th>TOTAL</th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td rowSpan={3}><img className='image' src="../public/images/tenis.png" alt="" /></td>
+                        <td>Tênis Nike Revolution 6 Next Nature Masculino</td>
+                        <td>+ / -</td>
+                        <td className='old-price'>R$ 219,00</td>
+                        <td className='old-price'>R$ 219,00</td>
+                    </tr>
+                    <tr>
+                        <td>Cor: Vermelho / Branco</td>
+                        <td>Remover item</td>
+                        <td>R$ 219,00</td>
+                        <td>R$ 219,00</td>
+                    </tr>
+                    <tr>
+                        <td>Tamanho: 42</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
+        
     )
 }
 
