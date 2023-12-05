61c79440dc35cb80ab384f03459c52a67804c8b7
Pedro Marino <pedro-mvlima@hotmail.com>
2023-12-05 20:01:08 -0300

    Feito a correção do espaçamento das linhas, tirou o <br> e adicionou width ao css.

--- src/components/OfferCard.jsx
+++ src/components/OfferCard.jsx
@@ -15,10 +15,7 @@ export const OfferCard = () => {
                     Air Jordan edição de <br />colecionador
                 </h1>
                 <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.   
-                    <br />Consequatur, aperiam repellat. Soluta maiores culpa expedita 
-                    <br /> nostrum facilis tempora rem perspiciatis perferendis tenetur 
-                    <br />dignissimos nemo, sit laborum aliquam doloremque iste mollitia!  
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur, aperiam repellat. Soluta maiores culpa expedita nostrum facilis tempora rem perspiciatis perferendis tenetur dignissimos nemo, sit laborum aliquam doloremque iste mollitia!  
                 </p>
                 <button>Ver Oferta</button>
             </div>
