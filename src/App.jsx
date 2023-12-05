7d0b56e35221a97489427432d32cc8d6cf152681
Luiz Felipe <felipe.sousapaiva03@gmail.com>
2023-12-05 19:35:58 -0300

    adicionando footer em react ao projeto

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+import { HomePage } from './pages/HomePage/HomePage'
+
+export function App() {
+  return (
+    <>
+      <HomePage />
+    </>
+  )
+}
---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,10 @@
+import { MainFooter } from "../../components/MainFooter/MainFooter";
+import "./HomePage.css"
+
+export const HomePage = () => {
+    return (
+        <div>
+            <MainFooter />
+        </div>
+    )
+}
