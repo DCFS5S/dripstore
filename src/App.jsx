17a5733d076c43aa6aee9dd2caa67dd6f7253813
Prof. Carlos <carlosdigitalcollege16@gmail.com>
2023-12-07 19:37:21 -0300

    Adiciona sistema de rotas ao projeto

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-import './App.css'
-import React from 'react'
-import { HomePage } from './pages/HomePage/HomePage'
-
-export function App() {
-  return (
-      <HomePage />
-  )
-}
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,9 @@
-import { OfferCard } from "../../components/OfferCard"
-import { CartCheckList } from "../../components/CartCheckList/CartCheckList"
+import { OfferCard } from '../../components/OfferCard/OfferCard'
 
 export function HomePage() {
     return (
       <>
         <OfferCard />
-        <CartCheckList name="Tênis Nike Revolution 6 Next Nature Masculino" color="Vermelho / Branco" size="42" price="219,00" oldPrice="219,00"/>
       </>
     )
   }
