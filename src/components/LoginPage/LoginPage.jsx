b7e4b4723cce5162e76905029280d812ffd74d03
Francisco Matheus Barros Teixeira <francisco0320@digitalcollegesul.local>
2024-01-09 20:32:58 -0300

    construcao da branch login

---
+++ src/components/LoginPage/LoginPage.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import './LoginPage.css'
+
+
+export const LoginPage = () => {
+
+    return (
+        <>
+        <div className='body-login'>   
+            <div className='login-wrapper'>
+                <div className='formulario-login'>
+            <div className='login-header'>
+                <h1>Acesse sua conta</h1>
+                <p>Novo cliente? Então registre-se <a href="#">aqui</a></p>
+            </div> 
+            <div className='login-body'>
+                <div>
+                    <label className='labels' htmlFor="username">Login*</label>
+                    <input type="text" placeholder='Insira seu login ou email' />
+                </div>
+                <div> 
+                    <label className='labels' htmlFor="password">Senha*</label>
+                    <input type="password" name="password" id="pass" minLength={8} required placeholder='Insira sua senha' />
+                </div>
+
+                <a className='forgot-password' href="#">Esqueci minha senha</a>
+                <button>Acessar Conta</button>
+            </div>
+            <div className='login-footer'>
+                <h4>Ou faça login com</h4>
+                <a href="#"><img src="../images/gmail-icon.png" alt="icone-gmail" /></a>
+                <a href="#"><img src="../images/fb-icon.png" alt="icone-facebook" /></a>
+            </div>
+            </div>
+            <div>
+                <img src="../images/tenis-juntos.png" alt="tenis-imagem" />
+            </div>
+            </div>           
+        </div>
+        </>
+    )
+}
