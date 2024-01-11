bb281bdf6d32c8e6b7367074e705ccfd3261e430
Eduardo Lopes <montalverne.edu@gmail.com>
2024-01-11 20:35:23 -0300

    feat(wip): Início da tag de média de avaliação e da descrição do produto com parágrafo

--- src/components/ProductDetailsComponent/ProductDetailsComponent.jsx
+++ src/components/ProductDetailsComponent/ProductDetailsComponent.jsx
@@ -1,4 +1,6 @@
 import './ProductDetailsComponent.css'
+import blankStar from '../../../img/blank-star.svg'
+
 export const ProductDetailsComponent = () => {
    
 
@@ -33,16 +35,16 @@ export const ProductDetailsComponent = () => {
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
                         <path d="M8.15102 1.2306L10.0003 5.65038L14.7346 6.06108C15.063 6.08972 15.1966 6.50343 14.9473 6.72122L11.3563 9.86292L12.4324 14.5367C12.507 14.8614 12.1585 15.1169 11.8764 14.9443L7.80774 12.4666L3.73906 14.9443C3.45622 15.1162 3.10846 14.8607 3.18309 14.5367L4.25921 9.86292L0.667401 6.72047C0.418146 6.50268 0.550983 6.08896 0.880089 6.06033L5.61444 5.64962L7.46371 1.2306C7.59206 0.923134 8.02266 0.923134 8.15102 1.2306Z" stroke="#F6AA1C"/>
                     </svg>
-
-                    
+                    <div className='rate'>
+                        <span>4.7</span>
+                        <img src={blankStar} alt="" />
+                    </div>
                 </div>
-
-
-
-
-</div>
-
-
+            </div>
+            <div>
+                <h3>Descrição do produto</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+            </div>
         </>
     )
 }
