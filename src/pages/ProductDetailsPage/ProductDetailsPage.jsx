8015b7245d42cd5191b79f42a3392d809da8b56d
Wiulis <wiulismonteiro56@gmail.com>
2024-01-11 18:59:56 -0300

    wip: Criação de componentes

--- src/pages/ProductDetailsPage/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage/ProductDetailsPage.jsx
@@ -1,11 +1,13 @@
 import { useParams } from 'react-router-dom'
+import { ProductDetailsComponent } from '../../ProductDetailsComponent/ProductDetailsComponent'
 
 export const ProductDetailsPage = () => {
     const { productName } = useParams()
 
     return (
-        <>
-        { productName }
-        </>
+        <div style={{display:'flex'}}>
+            <div style={{width: '50%'}}></div>
+            <ProductDetailsComponent/>
+        </div>
     )
 }
