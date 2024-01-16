718f925376c60beb1a0dc187c2e8b6513ea0e5b4
Wiulis De Araujo Monteiro <wiulis6123@digitalcollegesul.local>
2024-01-16 20:31:21 -0300

    logica

--- src/components/ProductImageCarrousel/ProductImageCarrousel.jsx
+++ src/components/ProductImageCarrousel/ProductImageCarrousel.jsx
@@ -1,21 +1,36 @@
-import sneaker from '../../../img/White-Sneakers-Nike.png'
-import sneaker2 from '../../../img/Nike.png'
 import './ProductImageCarrousel.css'
+import products from '../../../products.json'
+import { useState } from 'react'
+
+const product = products[0]
+
 
 export const ProductImageCarrousel = () => {
+    const [currentSrc,setCurrentSrc] = useState(product.images[0])
+
     return (
         <div className='carrouselContainer'>
             <div className='currentImage'>
-                <img src={sneaker} alt="" />
-                <button className='right'> &lt; </button>
-                <button className='left'> &gt; </button>
+                <button
+                    className='sliderArrows'
+                    onClick={() => {
+                        const index = product.images.indexOf(currentSrc) - 1
+                        setCurrentSrc(product.images[index])
+                    }}
+                ></button>
+                <img src={currentSrc} alt="" />
+                <button
+                    className='sliderArrows right'
+                ></button>
             </div>
             <div className='galleryImages'>
-                <img src={sneaker} alt="" />
-                <img src={sneaker2} alt="" />
-                <img src={sneaker2} alt="" />
-                <img src={sneaker2} alt="" />
-                <img src={sneaker2} alt="" />
+                {product.images.map((imageSrc,index) => 
+                    <img
+                        key={index}
+                        src={imageSrc}
+                        onClick={() => setCurrentSrc(imageSrc)}
+                    />
+                )}
             </div>
         </div>
     )
