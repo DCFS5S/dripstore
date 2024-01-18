d3b6332c1b888896fa61bd4b4f31506023e2ff7f
Wiulis De Araujo Monteiro <wiulis6123@digitalcollegesul.local>
2024-01-18 20:25:08 -0300

    Adicionando novos componentes e resetcss

--- src/pages/ProductDetailsPage/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage/ProductDetailsPage.jsx
@@ -6,8 +6,17 @@ export const ProductDetailsPage = () => {
     const { productName } = useParams()
 
     return (
-        <div style={{position: 'relative', padding: '7%', width: '97dvw'}}>
-            <ProductImageCarrousel />
-        </div>
+        <>
+            <div className='fullPath'>
+                <a href="">Home</a>
+                <a href="">Produtos</a>
+                <a href="">Tênis</a>
+                <a href="">Nike</a>
+                <a href="">Tênis Nike Revolution 6 Next Nature Masculino</a>
+            </div>
+            <div style={{position: 'relative', padding: '7%', width: '97dvw'}}>
+                <ProductImageCarrousel />
+            </div>
+        </>
     )
 }
