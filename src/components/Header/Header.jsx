56dda709dd209ef7d8484fcbadc3b0b6f9cc5dcd
lucasrochabz <lucasbezerrar@gmail.com>
2024-05-12 19:02:20 -0300

    Add links

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,31 @@
+import { Link } from 'react-router-dom'
 import './Header.css'
 
 export const Header = () => {
     return (
         <div className="main-header">
-             <div className="main-header-content">
-                <img className="logo" src="../../src/assets/logo.png" alt="" />
-                    <div className="search-bar">
-                        <input placeholder="Pesquisar produto..." className="main-header-search" type="search"/>
-                        <img className="search-lupa" src="../../src/assets/Search.svg" alt=""/>
-            </div>
-                <a href="" className="register-link">Cadastre-se</a>
-                <a href="" className="button" >Entrar</a>
-                <button className="kart-button">
-                <a href="#"><img src="../../src/assets/Buy.svg" alt="" /></a>
-                <span>2</span>
-                </button>
+            <div className="main-header-content">
+                <Link to='/'>
+                    <img className="logo" src="../../src/assets/logo.png" alt="" />
+                </Link>
+
+                <div className="search-bar">
+                    <input placeholder="Pesquisar produto..." className="main-header-search" type="search"/>
+                    <img className="search-lupa" src="../../src/assets/Search.svg" alt=""/>
+                </div>
+                
+                <Link to='/criar-conta' className='register-link'>
+                    Cadastre-se
+                </Link>
+
+                <Link to='/login' className='button'>
+                    Entrar
+                </Link>
+
+                <Link to='/carrinho' className='kart-button'>
+                    <img src="../../src/assets/Buy.svg" alt="Carrinho" />
+                    <span>2</span>
+                </Link>
             </div>
                 <nav className="main-header-menu">
                     <ul>
